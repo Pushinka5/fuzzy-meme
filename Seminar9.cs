7184340f038f4e7cd92a7af8755914b9498e80a1
Panasenko_Elena <panasenko.elena94@gmail.com>
2023-04-23 13:47:26 +0700

    prorgam main

--- Seminar9.cs
+++ Seminar9.cs
@@ -1,11 +1,17 @@
-Console.Clear();
-Console.Write("Введите N: ");
-int n = int.Parse(Console.ReadLine());
-
-Console.WriteLine(PrintNumbers(n));
-
-string PrintNumbers(int number)
+internal class Program
 {
-    if (number == 1) return "1";
-    return (number + " " + RrintNumbers(number - 1));
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введите N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(PrintNumbers(n));
+
+        string PrintNumbers(int number)
+        {
+            if (number == 1) return "1";
+            return number + " " + RrintNumbers(number - 1);
+        }
+    }
 }
